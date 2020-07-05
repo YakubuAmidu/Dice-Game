10a67f11c6009495216936eda683126d076d3d28
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-05 04:33:40 -0700

    Commiting changes

--- app.js
+++ app.js
@@ -1,39 +1,32 @@
+/*
+GAME RULES:
 
-    // 3. Update the round score if the rolled number is Not 1
-    if (dice !== 1){
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        // Next player
-        nextplayer();
-    }
-    }
-});
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
 
 
-document.querySelector('.btn-hold').addEventListener('click', function(){
+
+var scores, roundScore, activePlayer, nextPlayer, gamePlaying;
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
     if(gamePlaying){
+        //Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-             //Add current score to global score
-    scores[activePlayer] += roundScore;
+    // 2. Display the result
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
 
-    // update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    // check if the player won the game
-     if(scores[activePlayer] >= 100){
-         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-         document.querySelector('.dice').style.display = 'none';
-         document.querySelector('.player-' + activePlayer + '-panel').classList.add('Winner');
-         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-         gamePlaying = false;
-     } else {
-         // Next player
-    nextPlayer();
-     }
-    }
-});
+    
 
 function nextPlayer(){
     // Next player
